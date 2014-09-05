553b1ef039861c822bedf392167dee64d1001e4a
Troy Will <troydwill@gmail.com>
2014-09-05 16:09:28 -0700

    bin/mysql-setup.sh zoneminder/PKGBUILD

---
+++ bin/mysql-setup.sh
@@ -0,0 +1,5 @@
+#!/usr/bin/bash
+
+mysqladmin --defaults-file=/etc/mysql/my.cnf -p -f reload
+cat /usr/share/zoneminder/db/zm_create.sql | mysql --defaults-file=/etc/mysql/my.cnf -p
+echo 'grant lock tables, alter,select,insert,update,delete on zm.* to 'zmuser'@localhost identified by "zmpass";' | mysql --defaults-file=/
