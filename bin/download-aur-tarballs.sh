5e0bf852ee70ec0ee3dd9c57b5aeb97b7361ebd9
Troy Will <troydwill@gmail.com>
2014-09-05 14:18:51 -0700

    bin/make.pre.zoneminder.install.backups

--- bin/download-aur-tarballs.sh
+++ bin/download-aur-tarballs.sh
@@ -3,9 +3,10 @@
 # script to download Zoneminder dependency build tarballs from AUR
 set -o errexit
 cd ../aur
-wget https://aur.archlinux.org/packages/pe/perl-sys-mmap/perl-sys-mmap.tar.gz
 wget https://aur.archlinux.org/packages/ca/cambozola/cambozola.tar.gz
+wget https://aur.archlinux.org/packages/pe/perl-astro-suntime/perl-astro-suntime.tar.gz
 wget https://aur.archlinux.org/packages/pe/perl-expect/perl-expect.tar.gz
-wget https://aur.archlinux.org/packages/pe/perl-php-serialization/perl-php-serialization.tar.gz
 wget https://aur.archlinux.org/packages/pe/perl-net-sftp-foreign/perl-net-sftp-foreign.tar.gz
+wget https://aur.archlinux.org/packages/pe/perl-php-serialization/perl-php-serialization.tar.gz
+wget https://aur.archlinux.org/packages/pe/perl-sys-mmap/perl-sys-mmap.tar.gz
 wget https://aur.archlinux.org/packages/pe/perl-x10/perl-x10.tar.gz
