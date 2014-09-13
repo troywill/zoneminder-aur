6cc0086a9e732e850960afef73c647b6ae217311
Troy Will <troydwill@gmail.com>
2014-09-13 11:27:32 -0700

    bin/mysql-setup.sh: restart mysqld httpd zoneminder foscam-zoneminder.org: step by step instructions for a Foscam F18910W camera zoneminder-aur.org: open_basedir /var/cache/zoneminder

--- bin/mysql-setup.sh
+++ bin/mysql-setup.sh
@@ -2,4 +2,4 @@
 
 mysqladmin --defaults-file=/etc/mysql/my.cnf -p -f reload
 cat /usr/share/zoneminder/db/zm_create.sql | mysql --defaults-file=/etc/mysql/my.cnf -p
-echo 'grant lock tables, alter,select,insert,update,delete on zm.* to 'zmuser'@localhost identified by "zmpass";' | mysql --defaults-file=/
+echo 'grant lock tables, alter,select,insert,update,delete on zm.* to 'zmuser'@localhost identified by "zmpass";' | mysql --defaults-file=/mysqladmin --defaults-file=/etc/mysql/my.cnf -p -f reload
