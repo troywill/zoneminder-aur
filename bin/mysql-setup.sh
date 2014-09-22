a26ad2b56b33ac2585e7e469127c7f95d71ca039
Troy Will <troydwill@gmail.com>
2014-09-22 03:22:51 -0700

    zoneminder-aur.org: refactor for zoneminder-git package

--- bin/mysql-setup.sh
+++ bin/mysql-setup.sh
@@ -1,5 +1,6 @@
 #!/usr/bin/bash
 
+set -o verbose
 mysqladmin --defaults-file=/etc/mysql/my.cnf -p -f reload
 cat /usr/share/zoneminder/db/zm_create.sql | mysql --defaults-file=/etc/mysql/my.cnf -p
-echo 'grant lock tables, alter,select,insert,update,delete on zm.* to 'zmuser'@localhost identified by "zmpass";' | mysql --defaults-file=/mysqladmin --defaults-file=/etc/mysql/my.cnf -p -f reload
+echo 'grant lock tables, alter,select,insert,update,delete on zm.* to 'zmuser'@localhost identified by "zmpass";' | mysql --defaults-file=/etc/mysql/my.cnf -p mysql
